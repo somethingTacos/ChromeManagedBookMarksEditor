d260476e6c9edeff4ed0dddd211e2744ba852bc9
John Omelko <somethingtacos@hotmail.com>
2022-01-26 11:49:03 -0500

    Add avalonia project, archive old project, remove installer, updated readme

---
+++ ChromeManagedBookmarksEditor/ChromeManagedBookmarksEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChromeManagedBookmarksEditor.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ChromeManagedBookMarksEditor
 
 A program to edit the Chrome ManagedBookmarks JSON code for use in Group Policy.
-
-You can install this software using the 'ChromeManagedBookmarksEditor_Installer.msi'
